f9bb8622cf028d84541431efa1bf661e07c32235
prabha-git <prabhakaran.mails@gmail.com>
2023-11-30 00:06:09 -0600

    removed keys

--- app.py
+++ app.py
@@ -57,10 +57,4 @@ def main():
                 # log_error(e)  # Implement this function as needed
 
 if __name__ == "__main__":
-    os.environ['LANGCHAIN_TRACING_V2'] = 'true'
-    os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
-    os.environ['LANGCHAIN_API_KEY'] = "ls__e26dc1f5123d405bb8fe98749961be84"
-    os.environ['LANGCHAIN_PROJECT'] = "first_project"
-    os.environ['OPENAI_API_KEY'] = "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"
-    os.environ['LANGCHAIN_HUB_API_KEY'] = 'ls__7d4d6f5371414fe3a3d5e779ee29e143'
     main()
